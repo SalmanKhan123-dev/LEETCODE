dea0f77d05b1f8caf238f7f68526742fe3394caf
SalmanKhan123-dev <khamsalman73@gmail.com>
2026-07-24 19:19:05 +0530

    Time: 0 ms (100%), Space: 42.7 MB (99.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,42 +1,26 @@
-import java.util.Arrays;
-
-
 class Solution {
-   // Helper method to swap elements in the array
-   private static void swap(int[] nums, int a, int b) {
-       int temp = nums[a];
-       nums[a] = nums[b];
-       nums[b] = temp;
-   }
+    public void sortColors(int[] nums) {
+        int low=0;int high=nums.length-1;
+        int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
 
-
-   public static void sortColors(int[] nums) {
-       // Dutch National Flag algorithm
-       int low = 0, mid = 0, high = nums.length - 1;
-
-
-       // Loop through the array
-       while (mid <= high) {
-           if (nums[mid] == 0) {
-               // Move 0 to the beginning
-               swap(nums, low, mid);
-               low++;
-               mid++;
-           } else if (nums[mid] == 1) {
-               // Leave 1 in place
-               mid++;
-           } else {
-               // Move 2 to the end
-               swap(nums, mid, high);
-               high--;
-           }
-       }
-   }
-
-
-   public static void main(String[] args) {
-       int[] nums = {2, 0, 2, 1, 1, 0};
-       sortColors(nums);
-       System.out.println("Sorted colors: " + Arrays.toString(nums));
-   }
+            
+        }
+        
+    }
 }
